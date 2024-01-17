ec414f3612c925df2b8d4ed0f85caef2660653f5
mellatilaid <laidmellati@gmail.com>
2024-01-17 11:16:52 +0100

    generate type adaptor for note model

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/bottom_navigation_bar.dart';
 import 'package:note_app/helper/const.dart';
-import 'package:note_app/models/note_model.dart';
+
+import 'models/note_model.dart';
 
 void main() async {
   await Hive.initFlutter();
--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,13 +1,25 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   String title;
+  @HiveField(1)
   String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   int color;
+  @HiveField(4)
+  int? id;
 
   NoteModel({
     required this.title,
     required this.subTitle,
     required this.date,
     required this.color,
+    this.id,
   });
 }
