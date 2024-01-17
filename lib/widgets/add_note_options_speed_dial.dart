b8dda53515cebfd37499c06d2da340ddb3464f7e
mellatilaid <laidmellati@gmail.com>
2024-01-17 08:48:06 +0100

    add (add note options) to add note aptions speed dial

--- lib/widgets/add_note_options_speed_dial.dart
+++ lib/widgets/add_note_options_speed_dial.dart
@@ -14,21 +14,36 @@ class AddNoteOptionsSpeedDial extends StatelessWidget {
       icon: Icons.add,
       backgroundColor: kPrimaryColor,
       foregroundColor: Colors.white,
+      spaceBetweenChildren: 8,
       children: [
-        SpeedDialChild(
-          child: const Icon(Icons.text_fields),
-          // The background color of the mini FAB
-          backgroundColor: Colors.grey.withOpacity(0.6),
-          // The foreground color of the mini FAB
-          foregroundColor: Colors.white,
-          // The label of the mini FAB
-          label: 'Add text',
-          // The action to perform when the mini FAB is tapped
-          onTap: () {
-            // Add your logic for adding text here
-          },
+        customSpeedDialChild(
+          icon: const Icon(Icons.text_fields),
+          label: 'Add Text',
+        ),
+        customSpeedDialChild(
+          icon: const Icon(Icons.mic),
+          label: 'Add Voice',
+        ),
+        customSpeedDialChild(
+          icon: const Icon(Icons.image),
+          label: 'Add Image',
         ),
       ],
+    );
+  }
+
+  SpeedDialChild customSpeedDialChild(
+      {required Icon icon, required String label, VoidCallback? onTap}) {
+    return SpeedDialChild(
+      child: icon,
+      // The background color of the mini FAB
+      backgroundColor: Colors.grey.withOpacity(0.6),
+      // The foreground color of the mini FAB
+      foregroundColor: Colors.white,
+      // The label of the mini FAB
+      label: label,
+      // The action to perform when the mini FAB is tapped
+      onTap: onTap,
     );
   }
 }
