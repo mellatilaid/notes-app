aad4dc5f20a41749a887a88a6f7d6633be5c303f
mellatilaid <laidmellati@gmail.com>
2024-01-17 15:13:43 +0100

    create add note observer and add it to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/bottom_navigation_bar.dart';
+import 'package:note_app/cubits/add_note_cubit/simple_add_note_cubit_abserver.dart';
 import 'package:note_app/helper/const.dart';
 
 import 'models/note_model.dart';
 
 void main() async {
+  Bloc.observer = AddNoteObserver();
   await Hive.initFlutter();
   Hive.registerAdapter(NoteModelAdapter());
   await Hive.openBox<NoteModel>(kNoteBox);
