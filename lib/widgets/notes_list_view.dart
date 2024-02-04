7e9bfe7104129d122c5879197330d6d4b6109228
mellatilaid <laidmellati@gmail.com>
2024-02-04 09:04:26 +0100

    created slidable note and add it to notes list view

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:note_app/widgets/custom_note_item.dart';
 
 import '../models/note_model.dart';
-import 'custom_dismissible_note.dart';
 
 class NotesListView extends StatelessWidget {
   final List<NoteModel> notes;
@@ -13,13 +14,43 @@ class NotesListView extends StatelessWidget {
       padding: const EdgeInsets.all(8),
       itemCount: notes.length,
       itemBuilder: (context, index) {
-        return CustomDismissibleNote(
-          itemKey: ValueKey<int>(index),
-          notes: notes,
-          note: notes[index],
-          index: index,
-        );
+        return CustomSlidableNote(note: notes[index]);
       },
+    );
+  }
+}
+
+class CustomSlidableNote extends StatelessWidget {
+  const CustomSlidableNote({
+    super.key,
+    required this.note,
+  });
+
+  final NoteModel note;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Slidable(
+        startActionPane: ActionPane(motion: const StretchMotion(), children: [
+          SlidableAction(
+            onPressed: (context) {},
+            backgroundColor: const Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+          SlidableAction(
+            onPressed: (context) {},
+            backgroundColor: const Color(0xFF21B7CA),
+            foregroundColor: Colors.white,
+            icon: Icons.share,
+            label: 'Share',
+          ),
+        ]),
+        child: CustomNoteItem(note: note),
+      ),
     );
   }
 }
