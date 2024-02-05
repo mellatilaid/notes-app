f1bb5947ef45522670521404b0daac2f71665edd
mellatilaid <laidmellati@gmail.com>
2024-02-05 10:55:56 +0100

    create search notes view

---
+++ lib/views/search_notes_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchNotesView extends StatelessWidget {
+  const SearchNotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
