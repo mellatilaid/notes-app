a150ac47fc082f00e17efa4dbccea6d15b89ffd6
mellatilaid <laidmellati@gmail.com>
2024-02-06 08:51:30 +0100

    create search notes cubit

---
+++ lib/cubits/search_note_cubit/search_note_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:note_app/cubits/search_note_cubit/search_note_state.dart';
+
+import '../../helper/const.dart';
+import '../../models/note_model.dart';
+
+class SearchNoteCubit extends Cubit<SearchNoteState> {
+  SearchNoteCubit() : super(SearchInitialState());
+
+  searchNotes(String query) {
+    emit(SearchInitialState());
+
+    var notesBox = Hive.box<NoteModel>(kNoteBox);
+
+    List<NoteModel> notesFromBox = notesBox.values.toList();
+
+    if (query.isNotEmpty) {
+      final suggestions = notesFromBox.where((note) {
+        final noteTitle = note.title.toLowerCase();
+        final input = query.toLowerCase();
+        return noteTitle.contains(input);
+      }).toList();
+      emit(SearchSecussState(notes: suggestions));
+    }
+  }
+}
