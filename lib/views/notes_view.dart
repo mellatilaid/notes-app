bbf8cf89e5d3e057cfbc3e0780aa727a7f9aa9b0
mellatilaid <laidmellati@gmail.com>
2024-02-11 13:38:27 +0100

    add custom dial child method to note view floating action button

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/extensions/push_navigation_extension.dart';
+import 'package:note_app/helper/custom_speed_dial_child.dart';
+import 'package:note_app/views/add_notes_view.dart';
 import 'package:note_app/views/search_notes_view.dart';
 import 'package:note_app/widgets/add_note_options_speed_dial.dart';
 import 'package:note_app/widgets/notes_view_body.dart';
@@ -26,8 +28,20 @@ class MyNotesView extends StatelessWidget {
       ),
       drawer: const Drawer(),
       body: const NotesViewBody(),
-      floatingActionButton: const AddNoteOptionsSpeedDial(
-        dialChilds: [],
+      floatingActionButton: AddNoteOptionsSpeedDial(
+        dialChilds: [
+          customSpeedDialChild(
+            icon: const Icon(Icons.text_decrease),
+            label: 'Add Text',
+            onTap: () {
+              context.toView(const AddNotesView());
+            },
+          ),
+          customSpeedDialChild(
+              icon: const Icon(Icons.voice_chat), label: 'Add Voice'),
+          customSpeedDialChild(
+              icon: const Icon(Icons.photo), label: 'Add Photo'),
+        ],
       ),
     );
   }
--- lib/widgets/add_note_options_speed_dial.dart
+++ lib/widgets/add_note_options_speed_dial.dart
@@ -29,19 +29,4 @@ class AddNoteOptionsSpeedDial extends StatelessWidget {
       },
     );
   }
-
-  SpeedDialChild customSpeedDialChild(
-      {required Icon icon, required String label, VoidCallback? onTap}) {
-    return SpeedDialChild(
-      child: icon,
-      // The background color of the mini FAB
-      backgroundColor: Colors.grey.withOpacity(0.6),
-      // The foreground color of the mini FAB
-      foregroundColor: Colors.white,
-      // The label of the mini FAB
-      label: label,
-      // The action to perform when the mini FAB is tapped
-      onTap: onTap,
-    );
-  }
 }
