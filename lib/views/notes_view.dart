dc165aa6eb16d2bf50c610dfc0d0ae63f8e70848
mellatilaid <laidmellati@gmail.com>
2024-02-11 13:44:23 +0100

    removed custom dial child method

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/extensions/push_navigation_extension.dart';
-import 'package:note_app/helper/custom_speed_dial_child.dart';
-import 'package:note_app/views/add_notes_view.dart';
 import 'package:note_app/views/search_notes_view.dart';
 import 'package:note_app/widgets/add_note_options_speed_dial.dart';
 import 'package:note_app/widgets/notes_view_body.dart';
@@ -28,21 +26,7 @@ class MyNotesView extends StatelessWidget {
       ),
       drawer: const Drawer(),
       body: const NotesViewBody(),
-      floatingActionButton: AddNoteOptionsSpeedDial(
-        dialChilds: [
-          customSpeedDialChild(
-            icon: const Icon(Icons.text_decrease),
-            label: 'Add Text',
-            onTap: () {
-              context.toView(const AddNotesView());
-            },
-          ),
-          customSpeedDialChild(
-              icon: const Icon(Icons.voice_chat), label: 'Add Voice'),
-          customSpeedDialChild(
-              icon: const Icon(Icons.photo), label: 'Add Photo'),
-        ],
-      ),
+      floatingActionButton: const AddNoteOptionsSpeedDial(),
     );
   }
 }
--- lib/widgets/add_note_options_speed_dial.dart
+++ lib/widgets/add_note_options_speed_dial.dart
@@ -3,14 +3,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/cubits/notes_cubit/notes_cubit_states.dart';
+import 'package:note_app/extensions/push_navigation_extension.dart';
+import 'package:note_app/views/add_notes_view.dart';
 
 import '../helper/const.dart';
 
 class AddNoteOptionsSpeedDial extends StatelessWidget {
-  final List<SpeedDialChild> dialChilds;
   const AddNoteOptionsSpeedDial({
     super.key,
-    required this.dialChilds,
   });
 
   @override
@@ -24,9 +24,39 @@ class AddNoteOptionsSpeedDial extends StatelessWidget {
           foregroundColor: Colors.white,
           buttonSize: const Size(50, 50),
           spaceBetweenChildren: 8,
-          children: dialChilds.map((e) => e).toList(),
+          children: [
+            customSpeedDialChild(
+                icon: const Icon(Icons.text_fields),
+                label: 'Add Text',
+                onTap: () {
+                  context.toView(const AddNotesView());
+                }),
+            customSpeedDialChild(
+              icon: const Icon(Icons.mic),
+              label: 'Add Voice',
+            ),
+            customSpeedDialChild(
+              icon: const Icon(Icons.image),
+              label: 'Add Image',
+            ),
+          ],
         );
       },
+    );
+  }
+
+  SpeedDialChild customSpeedDialChild(
+      {required Icon icon, required String label, VoidCallback? onTap}) {
+    return SpeedDialChild(
+      child: icon,
+      // The background color of the mini FAB
+      backgroundColor: Colors.grey.withOpacity(0.6),
+      // The foreground color of the mini FAB
+      foregroundColor: Colors.white,
+      // The label of the mini FAB
+      label: label,
+      // The action to perform when the mini FAB is tapped
+      onTap: onTap,
     );
   }
 }
