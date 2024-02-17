292abd9d29e0b77ecc78262d4480cd4a065d49c6
mellatilaid <laidmellati@gmail.com>
2024-02-17 10:11:30 +0100

    refactor folders grid view

--- lib/widgets/notes_folders_grid_view.dart
+++ lib/widgets/notes_folders_grid_view.dart
@@ -6,21 +6,16 @@ class FoldersGridView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 8),
+      padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 8),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3,
-        //childAspectRatio: 1.6,
-        crossAxisSpacing: 5,
+        crossAxisSpacing: 20,
         mainAxisSpacing: 30,
+        childAspectRatio: 0.7,
       ),
       itemCount: 10,
       itemBuilder: (context, index) {
-        return const SizedBox(
-          height: 200,
-          child: Card(
-            child: Center(child: Text('laid')),
-          ),
-        );
+        return const Text('oops');
       },
     );
   }
