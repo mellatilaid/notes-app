410f0c7335296ab3216746458b7b7b02fee79075
mellatilaid <laidmellati@gmail.com>
2024-02-19 11:25:46 +0100

    add hive fields to folder model

--- lib/models/folder_model.dart
+++ lib/models/folder_model.dart
@@ -1,10 +1,17 @@
+import 'package:hive/hive.dart';
 import 'package:note_app/models/note_model.dart';
 
+@HiveType(typeId: 1)
 class FolderModel {
+  @HiveField(0)
   String title;
+  @HiveField(1)
   String? coverPath;
+  @HiveField(2)
   int color;
+  @HiveField(3)
   List<NoteModel> notes;
+  @HiveField(4)
   List<FolderModel> folders;
 
   FolderModel({
