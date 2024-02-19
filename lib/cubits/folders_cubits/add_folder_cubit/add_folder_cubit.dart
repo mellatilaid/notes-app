45ce55c518535c1b9a11801c7c6b5cbbb612999e
mellatilaid <laidmellati@gmail.com>
2024-02-19 11:37:43 +0100

    create add folder cubit

---
+++ lib/cubits/folders_cubits/add_folder_cubit/add_folder_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:note_app/helper/const.dart';
+import 'package:note_app/models/folder_model.dart';
+
+import 'add_folder_states.dart';
+
+class AddFolderCubit extends Cubit<AddFolderCubitState> {
+  AddFolderCubit() : super(InitialState());
+
+  addFolder({required FolderModel folder}) async {
+    emit(AddFolderLoadingState());
+
+    try {
+      final foldersBox = Hive.box<FolderModel>(kFoldersBox);
+      await foldersBox.add(folder);
+      emit(SucussAddFolderState());
+    } catch (e) {
+      emit(FailureAddFolderState(errMessage: e.toString()));
+    }
+  }
+}
