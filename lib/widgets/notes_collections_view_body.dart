2157ead2d0d96d8a9d9bea4026a3684bfecc7f19
mellatilaid <laidmellati@gmail.com>
2024-02-25 18:18:10 +0100

    integrate folder cubit in notes collections view body

--- lib/widgets/notes_collections_view_body.dart
+++ lib/widgets/notes_collections_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/cubits/folders_cubits/fetch_folders_cubit/folders_cubit.dart';
+import 'package:note_app/cubits/folders_cubits/fetch_folders_cubit/folders_cubit_states.dart';
 
 import 'notes_folders_grid_view.dart';
 
@@ -22,6 +23,16 @@ class _NotesCollectionsViewBodyState extends State<NotesCollectionsViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return const FoldersGridView();
+    return BlocBuilder<FoldersCubit, FolderState>(
+      builder: (context, state) {
+        if (state is FoldersInitialState) {
+          return const Text('Add Your Folders');
+        } else if (state is FoldersSucussState) {
+          return FoldersGridView(folders: state.folders);
+        } else {
+          return const Text('Oops There Was A Problem');
+        }
+      },
+    );
   }
 }
--- lib/widgets/notes_folders_grid_view.dart
+++ lib/widgets/notes_folders_grid_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/models/folder_model.dart';
 import 'package:note_app/widgets/custom_folder_item.dart';
 
 class FoldersGridView extends StatelessWidget {
-  const FoldersGridView({super.key});
+  final List<FolderModel> folders;
+  const FoldersGridView({super.key, required this.folders});
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +16,7 @@ class FoldersGridView extends StatelessWidget {
         mainAxisSpacing: 30,
         childAspectRatio: 0.6,
       ),
-      itemCount: 10,
+      itemCount: folders.length,
       itemBuilder: (context, index) {
         return const CustomFolderItem();
       },
