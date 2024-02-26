b9de89150b0cb7f74b723a3b95c6021da72689cc
mellatilaid <laidmellati@gmail.com>
2024-02-26 14:23:55 +0100

    moved addfolder form widget to a seperate file

---
+++ lib/widgets/add_folder_form.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubits/folders_cubits/add_folder_cubit/add_folder_cubit.dart';
+import '../cubits/folders_cubits/add_folder_cubit/add_folder_states.dart';
+import '../cubits/folders_cubits/fetch_folders_cubit/folders_cubit.dart';
+import '../helper/const.dart';
+import '../models/folder_model.dart';
+import 'custom_action_button.dart';
+import 'custom_text_button.dart';
+import 'folders_colors_list_view.dart';
+import 'new_folder_text_field.dart';
+
+class AddFolderForm extends StatefulWidget {
+  const AddFolderForm({
+    super.key,
+    required TextEditingController folderTitleController,
+  }) : _folderTitleController = folderTitleController;
+
+  final TextEditingController _folderTitleController;
+
+  @override
+  State<AddFolderForm> createState() => _AddFolderFormState();
+}
+
+class _AddFolderFormState extends State<AddFolderForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          NewFolderTextField(controller: widget._folderTitleController),
+          const SizedBox(
+            height: 8,
+          ),
+          const FoldersColorsListView(),
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: Divider(
+                height: 2,
+                color: Colors.grey.withOpacity(0.5),
+              )),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: Text('Upload Folder Cover'),
+              ),
+              Expanded(
+                child: Divider(
+                  height: 2,
+                  color: Colors.grey.withOpacity(0.5),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          CustomTextButton(
+            title: 'Gallery',
+            onPressed: () {},
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          BlocBuilder<AddFolderCubit, AddFolderState>(
+            builder: (context, state) {
+              return CustomActionButton(
+                isLoading: (state is AddFolderLoadingState) ? true : false,
+                title: 'Create Folder',
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    final addFolderCubit = context.read<AddFolderCubit>();
+                    _addFolder(
+                      folderTitle: widget._folderTitleController.text,
+                      addFolderCubit: addFolderCubit,
+                    );
+                    BlocProvider.of<FoldersCubit>(context).fetchAllFolders();
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+                backGroundColor: kPrimaryColor,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  _addFolder(
+      {required String folderTitle, required AddFolderCubit addFolderCubit}) {
+    final FolderModel folder = FolderModel(
+      title: folderTitle,
+      color: kColors.first.value,
+      folders: [],
+      notes: [],
+    );
+    addFolderCubit.addFolder(folder: folder);
+  }
+}
--- lib/widgets/new_folder_bottom_sheet.dart
+++ lib/widgets/new_folder_bottom_sheet.dart
@@ -2,14 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/cubits/folders_cubits/add_folder_cubit/add_folder_cubit.dart';
 import 'package:note_app/cubits/folders_cubits/add_folder_cubit/add_folder_states.dart';
-import 'package:note_app/helper/const.dart';
-import 'package:note_app/models/folder_model.dart';
-import 'package:note_app/widgets/custom_action_button.dart';
-import 'package:note_app/widgets/custom_text_button.dart';
-import 'package:note_app/widgets/folders_colors_list_view.dart';
 
-import '../cubits/folders_cubits/fetch_folders_cubit/folders_cubit.dart';
-import 'new_folder_text_field.dart';
+import 'add_folder_form.dart';
 
 class NewFolderBottomSheet extends StatefulWidget {
   const NewFolderBottomSheet({super.key});
@@ -20,6 +14,13 @@ class NewFolderBottomSheet extends StatefulWidget {
 
 class _NewFolderBottomSheetState extends State<NewFolderBottomSheet> {
   final TextEditingController _folderTitleController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _folderTitleController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,106 +45,5 @@ class _NewFolderBottomSheetState extends State<NewFolderBottomSheet> {
         ),
       ),
     );
-  }
-}
-
-class AddFolderForm extends StatefulWidget {
-  const AddFolderForm({
-    super.key,
-    required TextEditingController folderTitleController,
-  }) : _folderTitleController = folderTitleController;
-
-  final TextEditingController _folderTitleController;
-
-  @override
-  State<AddFolderForm> createState() => _AddFolderFormState();
-}
-
-class _AddFolderFormState extends State<AddFolderForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          NewFolderTextField(controller: widget._folderTitleController),
-          const SizedBox(
-            height: 8,
-          ),
-          const FoldersColorsListView(),
-          const SizedBox(
-            height: 8,
-          ),
-          Row(
-            children: [
-              Expanded(
-                  child: Divider(
-                height: 2,
-                color: Colors.grey.withOpacity(0.5),
-              )),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8),
-                child: Text('Upload Folder Cover'),
-              ),
-              Expanded(
-                child: Divider(
-                  height: 2,
-                  color: Colors.grey.withOpacity(0.5),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          CustomTextButton(
-            title: 'Gallery',
-            onPressed: () {},
-          ),
-          const SizedBox(
-            height: 32,
-          ),
-          BlocBuilder<AddFolderCubit, AddFolderState>(
-            builder: (context, state) {
-              return CustomActionButton(
-                isLoading: (state is AddFolderLoadingState) ? true : false,
-                title: 'Create Folder',
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    final addFolderCubit = context.read<AddFolderCubit>();
-                    _addFolder(
-                      folderTitle: widget._folderTitleController.text,
-                      addFolderCubit: addFolderCubit,
-                    );
-                    BlocProvider.of<FoldersCubit>(context).fetchAllFolders();
-                  } else {
-                    autovalidateMode = AutovalidateMode.always;
-                    setState(() {});
-                  }
-                },
-                backGroundColor: kPrimaryColor,
-              );
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  _addFolder(
-      {required String folderTitle, required AddFolderCubit addFolderCubit}) {
-    final FolderModel folder = FolderModel(
-      title: folderTitle,
-      color: kColors.first.value,
-      folders: [],
-      notes: [],
-    );
-    addFolderCubit.addFolder(folder: folder);
   }
 }
