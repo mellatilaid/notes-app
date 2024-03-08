e7afa4e9c712c2cda93ce98ce5a9a19d4deb120d
mellatilaid <laidmellati@gmail.com>
2024-03-08 11:54:06 +0100

    add list view and add subfolder button to subfolder tab bar view

--- lib/widgets/folder_sub_folders_body.dart
+++ lib/widgets/folder_sub_folders_body.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 
+import '../helper/const.dart';
+import 'custom_action_button.dart';
+
 class FolderSubFoldersBody extends StatelessWidget {
   const FolderSubFoldersBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Sub Folders'),
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: 0,
+            itemBuilder: (context, index) {
+              return Container(
+                width: double.infinity,
+                height: 20,
+                color: Colors.blue,
+              );
+            },
+          ),
+        ),
+        CustomActionButton(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8,
+            vertical: 8,
+          ),
+          title: 'Add SubFolder',
+          onPressed: () {},
+          backGroundColor: kPrimaryColor,
+        ),
+      ],
     );
   }
 }
