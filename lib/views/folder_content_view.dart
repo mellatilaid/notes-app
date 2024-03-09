054f1679a53721da879efa7b0d085809b5c9a5f7
mellatilaid <laidmellati@gmail.com>
2024-03-09 11:43:07 +0100

    add integrate for sub notes cubit

--- lib/views/folder_content_view.dart
+++ lib/views/folder_content_view.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:note_app/cubits/folders_cubits/fetch_folder_subnotes_cubit/fetch_sub_notes_states.dart';
+import 'package:note_app/cubits/folders_cubits/fetch_folder_subnotes_cubit/fethc_sub_notes_cubit.dart';
 import 'package:note_app/extensions/push_navigation_extension.dart';
-import 'package:note_app/helper/const.dart';
 import 'package:note_app/models/folder_model.dart';
 import 'package:note_app/views/add_sub_note_view.dart';
 import 'package:note_app/widgets/add_note_options_speed_dial.dart';
-import 'package:note_app/widgets/folder_sub_folders_body.dart';
 
 import '../widgets/folder_subnotes_body.dart';
 
@@ -25,53 +26,47 @@ class FolderContentView extends StatefulWidget {
 class _FolderContentViewState extends State<FolderContentView> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: Text(widget.folder.title),
-          centerTitle: true,
-          bottom: const TabBar(
-            labelColor: kPrimaryColor,
-            indicatorColor: kPrimaryColor,
-            tabs: [
-              Tab(
-                text: 'Notes',
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(widget.folder.title),
+        centerTitle: true,
+      ),
+      body: BlocBuilder<SubNotesCubit, SubNotesState>(
+        builder: (context, state) {
+          if (state is InitialState) {
+            return const Center(
+              child: Text('Add Your Sub Notes'),
+            );
+          } else if (state is SucussState) {
+            return FolderSubNotesBody(
+              notes: state.notes,
+            );
+          } else {
+            return const Center(
+              child: Text('Oops there was a problem'),
+            );
+          }
+        },
+      ),
+      floatingActionButton: AddNoteOptionsSpeedDial(
+        speedDials: [
+          customSpeedDialChild(
+            onTap: () => context.toView(
+              AddSubNoteView(
+                folder: widget.folder,
               ),
-              Tab(
-                text: 'SubFolders',
-              ),
-            ],
+            ),
+            icon: const Icon(Icons.text_fields),
+            label: 'Add Text',
           ),
-        ),
-        body: TabBarView(
-          children: [
-            FolderSubNotesBody(
-              folder: widget.folder,
-            ),
-            const FolderSubFoldersBody(),
-          ],
-        ),
-        floatingActionButton: AddNoteOptionsSpeedDial(
-          speedDials: [
-            customSpeedDialChild(
-              onTap: () => context.toView(
-                AddSubNoteView(
-                  folder: widget.folder,
-                ),
-              ),
-              icon: const Icon(Icons.text_fields),
-              label: 'Add Text',
-            ),
-            customSpeedDialChild(
-                icon: const Icon(Icons.voice_chat), label: 'Add Voice'),
-            customSpeedDialChild(
-                icon: const Icon(Icons.image), label: 'Add Image'),
-            customSpeedDialChild(
-                icon: const Icon(Icons.folder), label: 'Add SubFolder'),
-          ],
-        ),
+          customSpeedDialChild(
+              icon: const Icon(Icons.voice_chat), label: 'Add Voice'),
+          customSpeedDialChild(
+              icon: const Icon(Icons.image), label: 'Add Image'),
+          customSpeedDialChild(
+              icon: const Icon(Icons.folder), label: 'Add SubFolder'),
+        ],
       ),
     );
   }
--- lib/widgets/folder_subnotes_body.dart
+++ lib/widgets/folder_subnotes_body.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/models/folder_model.dart';
+import 'package:note_app/models/note_model.dart';
 
 class FolderSubNotesBody extends StatefulWidget {
-  final FolderModel folder;
-  const FolderSubNotesBody({super.key, required this.folder});
+  final List<NoteModel> notes;
+  const FolderSubNotesBody({super.key, required this.notes});
 
   @override
   State<FolderSubNotesBody> createState() => _FolderSubNotesBodyState();
@@ -12,17 +12,15 @@ class FolderSubNotesBody extends StatefulWidget {
 class _FolderSubNotesBodyState extends State<FolderSubNotesBody> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: widget.folder.notes.length,
-        itemBuilder: (context, index) {
-          return Container(
-            width: double.infinity,
-            height: 20,
-            color: Colors.blue,
-          );
-        },
-      ),
+    return ListView.builder(
+      itemCount: widget.notes.length,
+      itemBuilder: (context, index) {
+        return Container(
+          width: double.infinity,
+          height: 20,
+          color: Colors.blue,
+        );
+      },
     );
   }
 }
