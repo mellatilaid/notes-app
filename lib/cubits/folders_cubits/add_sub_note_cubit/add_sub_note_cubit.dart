54431fa50dbe7317af84bdec680f2bc76f893f93
mellatilaid <laidmellati@gmail.com>
2024-03-14 14:25:02 +0100

    create add sub note cubit

---
+++ lib/cubits/folders_cubits/add_sub_note_cubit/add_sub_note_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/cubits/folders_cubits/add_sub_note_cubit/add_sub_note_states.dart';
+import 'package:note_app/helper/const.dart';
+import 'package:note_app/models/folder_model.dart';
+import 'package:note_app/models/note_model.dart';
+
+class AddSubNoteCubit extends Cubit<AddSubNoteState> {
+  AddSubNoteCubit() : super(InitialState());
+
+  addSubNote({required int folderIndex, required NoteModel note}) async {
+    emit(LoadingState());
+    final foldersBox = Hive.box<FolderModel>(kFoldersBox);
+
+    final folder = foldersBox.getAt(folderIndex);
+
+    if (folder != null) {
+      folder.notes.add(note);
+      try {
+        await foldersBox.put(folder.key, folder);
+        emit(SuccussState());
+      } catch (e) {
+        emit(FailureState(errMessage: e.toString()));
+      }
+    }
+  }
+}
