b65a923594119e47e6b29222664779df76757651
mellatilaid <laidmellati@gmail.com>
2024-03-29 01:17:34 +0100

    reverse  folder notes to display them newest to oldest

--- lib/widgets/folder_subnotes_body.dart
+++ lib/widgets/folder_subnotes_body.dart
@@ -11,14 +11,23 @@ class FolderSubNotesBody extends StatefulWidget {
 }
 
 class _FolderSubNotesBodyState extends State<FolderSubNotesBody> {
+  //reverce notes list to show notes newest to oldest one
+  late List<NoteModel> revNotes;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    revNotes = widget.notes.reversed.toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       padding: const EdgeInsets.all(8),
-      itemCount: widget.notes.length,
+      itemCount: revNotes.length,
       itemBuilder: (context, index) {
         return SubNotesViewSlidableNote(
-          note: widget.notes[index],
+          note: revNotes[index],
           noteIndex: index,
         );
       },
