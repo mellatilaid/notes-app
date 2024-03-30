974e901431a18636f08aecae8648c1211399aea5
mellatilaid <laidmellati@gmail.com>
2024-03-30 02:37:35 +0100

    build add voice note bottom sheet UI

--- lib/widgets/add_voice_note_bottom_sheet.dart
+++ lib/widgets/add_voice_note_bottom_sheet.dart
@@ -5,6 +5,51 @@ class AddVoiceNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            '00:00',
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          RecordVoiceActions()
+        ],
+      ),
+    );
+  }
+}
+
+class RecordVoiceActions extends StatelessWidget {
+  const RecordVoiceActions({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.cancel),
+        ),
+        CircleAvatar(
+            radius: 35,
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.play_arrow),
+              iconSize: 50,
+            )),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.done),
+        ),
+      ],
+    );
   }
 }
