59a90dab8bc637d899115ecf18e7a628934a6cbb
mellatilaid <laidmellati@gmail.com>
2024-03-30 02:17:27 +0100

    create add voice note view body

--- lib/views/add_voice_note_view.dart
+++ lib/views/add_voice_note_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/add_voice_note_view_body.dart';
 
 class AddVoiceNoteView extends StatelessWidget {
   const AddVoiceNoteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Add Voice'),
+      ),
+      body: const AddVoiceNoteViewBody(),
+    );
   }
 }
---
+++ lib/widgets/add_voice_note_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AddVoiceNoteViewBody extends StatelessWidget {
+  const AddVoiceNoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
