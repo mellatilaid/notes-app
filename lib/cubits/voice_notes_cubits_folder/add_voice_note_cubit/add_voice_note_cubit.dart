864ddb584f7401f5ad912b9fdee1cd7d9ba78516
mellatilaid <laidmellati@gmail.com>
2024-04-08 02:34:39 +0200

    create add voice note cubit

---
+++ lib/cubits/voice_notes_cubits_folder/add_voice_note_cubit/add_voice_note_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/cubits/voice_notes_cubits_folder/add_voice_note_cubit/add_vioce_note_cubit_states.dart';
+import 'package:note_app/helper/const.dart';
+import 'package:note_app/models/voice_note_model.dart';
+
+class AddVoiceNoteCubit extends Cubit<AddVoiceNotesState> {
+  AddVoiceNoteCubit() : super(InitialState());
+
+  addVoiceNote({required VoiceNoteModel voiceNoteModel}) async {
+    emit(AddVoiceNoteLoading());
+
+    try {
+      var voiceNoteBox = Hive.box<VoiceNoteModel>(kVoiceNoteBox);
+
+      await voiceNoteBox.add(voiceNoteModel);
+      emit(AddVoiceNoteSuccuss());
+    } catch (e) {
+      emit(AddVoiceNoteFailure(errMessage: e.toString()));
+    }
+  }
+}
