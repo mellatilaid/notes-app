19688141a2261ae474088767bc413411dc493d6a
mellatilaid <laidmellati@gmail.com>
2024-04-08 02:52:43 +0200

    provided add voice note cubit to add voice note bottom sheet

--- lib/widgets/add_voice_note_bottom_sheet.dart
+++ lib/widgets/add_voice_note_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/voice_notes_cubits_folder/add_voice_note_cubit/add_voice_note_cubit.dart';
 import 'package:note_app/widgets/add_voice_note_bottom_sheet_body.dart';
 
 class AddVoiceNoteBottomSheet extends StatelessWidget {
@@ -6,6 +8,9 @@ class AddVoiceNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const AddVoiceNoteBottomSheetBody();
+    return BlocProvider(
+      create: (context) => AddVoiceNoteCubit(),
+      child: const AddVoiceNoteBottomSheetBody(),
+    );
   }
 }
