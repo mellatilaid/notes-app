04e4ee9e4609031f04aee369d554083b7bba09f1
mellatilaid <laidmellati@gmail.com>
2024-04-15 22:52:13 +0200

    created voice notes builder to build the voice notes body based on voice notes cubit return

--- lib/widgets/voice_notes_view_body.dart
+++ lib/widgets/voice_notes_view_body.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/folders_cubits/add_folder_cubit/add_folder_states.dart';
 import 'package:note_app/cubits/voice_notes_cubits_folder/voice_notes_cubit/voice_notes_cubit.dart';
+import 'package:note_app/cubits/voice_notes_cubits_folder/voice_notes_cubit/voice_notes_cubit_states.dart';
 
-class VoiceNotesViewBody extends StatelessWidget {
+class VoiceNotesViewBody extends StatefulWidget {
   const VoiceNotesViewBody({super.key});
 
+  @override
+  State<VoiceNotesViewBody> createState() => _VoiceNotesViewBodyState();
+}
+
+class _VoiceNotesViewBodyState extends State<VoiceNotesViewBody> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => VoiceNotesCubit(),
-      child: Container(),
+      child: const VoiceNotesBuilder(),
     );
   }
 }
@@ -19,6 +26,28 @@ class VoiceNotesBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocBuilder<VoiceNotesCubit, VoiceNotesState>(
+      builder: (context, state) {
+        if (state is InitialState) {
+          return const Center(
+            child: Text('Add Your Voice Notes'),
+          );
+        } else if (state is VoiceNotesSuccussState) {
+          return ListView.builder(
+              itemCount: state.voiceNotes.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: double.infinity,
+                  height: 50,
+                  color: Colors.blue,
+                );
+              });
+        } else {
+          return const Center(
+            child: Text('oops there was a problem'),
+          );
+        }
+      },
+    );
   }
 }
