4cc0ce2fddf9051f7040adc5294b81be1840752c
mellatilaid <laidmellati@gmail.com>
2024-04-15 22:48:03 +0200

    provided voice note cubit to voice notes view body

--- lib/widgets/voice_notes_view_body.dart
+++ lib/widgets/voice_notes_view_body.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/voice_notes_cubits_folder/voice_notes_cubit/voice_notes_cubit.dart';
 
 class VoiceNotesViewBody extends StatelessWidget {
   const VoiceNotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('voice view'),
+    return BlocProvider(
+      create: (context) => VoiceNotesCubit(),
+      child: Container(),
     );
+  }
+}
+
+class VoiceNotesBuilder extends StatelessWidget {
+  const VoiceNotesBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
