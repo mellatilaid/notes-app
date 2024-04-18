2a9ddb4e4243ad14e4f4699b54978d835feaf230
mellatilaid <laidmellati@gmail.com>
2024-04-18 10:51:29 +0200

    added title to voice player view

--- lib/views/voice_player_view.dart
+++ lib/views/voice_player_view.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/models/voice_note_model.dart';
+import 'package:note_app/widgets/custom_text_field.dart';
 
-class VoicePlayerView extends StatelessWidget {
-  const VoicePlayerView({super.key});
+class VoicePlayerView extends StatefulWidget {
+  final VoiceNoteModel voiceNote;
+  const VoicePlayerView({
+    super.key,
+    required this.voiceNote,
+  });
+
+  @override
+  State<VoicePlayerView> createState() => _VoicePlayerViewState();
+}
+
+class _VoicePlayerViewState extends State<VoicePlayerView> {
+  late TextEditingController _titleController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _titleController = TextEditingController(text: widget.voiceNote.title);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _titleController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('voice palyer'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            CustomTextField(
+              controller: _titleController,
+              hintText: 'Title',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/custom_voice_note_item.dart
+++ lib/widgets/custom_voice_note_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/extensions/push_navigation_extension.dart';
+import 'package:note_app/views/voice_player_view.dart';
 import 'package:note_app/widgets/custom_rounded_icon.dart';
 
 import '../models/voice_note_model.dart';
@@ -9,48 +11,53 @@ class CustomVoiceNoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(
-          color: Colors.grey,
-          width: 2,
+    return GestureDetector(
+      onTap: () => context.toView(VoicePlayerView(
+        voiceNote: voiceNote,
+      )),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: Colors.grey,
+            width: 2,
+          ),
         ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              voiceNote.title,
-              textAlign: TextAlign.start,
-              style: const TextStyle(
-                fontSize: 16,
-                color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                voiceNote.title,
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const CustomRoundedIcon(
-                  icon: Icons.play_arrow,
-                  size: 30,
-                ),
-                Text(
-                  voiceNote.date!,
-                  style: const TextStyle(
-                    color: Colors.white,
+              const SizedBox(
+                height: 8,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const CustomRoundedIcon(
+                    icon: Icons.play_arrow,
+                    size: 30,
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Text(
+                    voiceNote.date!,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
