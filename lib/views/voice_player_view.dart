36292fe0169b5d778c4ef050f00f7575d5d52153
mellatilaid <laidmellati@gmail.com>
2024-04-18 10:41:26 +0200

    created voice player view

---
+++ lib/views/voice_player_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VoicePlayerView extends StatelessWidget {
+  const VoicePlayerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
