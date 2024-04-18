3464f64ade76efaf435059b8a30c56fbaaed13be
mellatilaid <laidmellati@gmail.com>
2024-04-18 11:01:43 +0200

    added audio player methods to voice player view

--- lib/views/voice_player_view.dart
+++ lib/views/voice_player_view.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:note_app/models/voice_note_model.dart';
 import 'package:note_app/widgets/custom_text_field.dart';
@@ -15,12 +16,39 @@ class VoicePlayerView extends StatefulWidget {
 
 class _VoicePlayerViewState extends State<VoicePlayerView> {
   late TextEditingController _titleController;
-
+  final audioPlayer = AudioPlayer();
+  bool isPlaying = false;
+  var duration = Duration.zero;
+  var position = Duration.zero;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _titleController = TextEditingController(text: widget.voiceNote.title);
+    setAudio();
+    audioPlayer.onPlayerStateChanged.listen((state) {
+      setState(() {
+        isPlaying = state == PlayerState.playing;
+      });
+    });
+
+    audioPlayer.onDurationChanged.listen((newDuration) {
+      setState(() {
+        duration = newDuration;
+      });
+    });
+
+    audioPlayer.onPositionChanged.listen((newPostion) {
+      setState(() {
+        position = newPostion;
+      });
+    });
+  }
+
+  setAudio() async {
+    final player = AudioCache(prefix: 'assets/');
+    final url = await player.load('zero.mp3');
+    audioPlayer.setSourceUrl(widget.voiceNote.voicePath);
   }
 
   @override
@@ -45,9 +73,57 @@ class _VoicePlayerViewState extends State<VoicePlayerView> {
               controller: _titleController,
               hintText: 'Title',
             ),
+            Slider(
+              min: 0,
+              max: duration.inSeconds.toDouble(),
+              value: position.inSeconds.toDouble(),
+              onChanged: (value) async {
+                final postion = Duration(seconds: value.toInt());
+                await audioPlayer.seek(postion);
+
+                await audioPlayer.resume();
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(formatDuration(duration.inMilliseconds)),
+                  Text(formatDuration(position.inMilliseconds)),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  if (isPlaying) {
+                    await audioPlayer.pause();
+                  } else {
+                    await audioPlayer.resume();
+                  }
+                },
+                child: Icon(
+                  isPlaying ? Icons.stop : Icons.play_arrow,
+                  size: 50,
+                ))
           ],
         ),
       ),
     );
+  }
+
+  String formatDuration(int milliseconds) {
+    int seconds = (milliseconds / 1000).floor();
+    int minutes = (seconds / 60).floor();
+    int hours = (minutes / 60).floor();
+
+    String hoursStr = (hours % 24).toString().padLeft(2, '0');
+    String minutesStr = (minutes % 60).toString().padLeft(2, '0');
+    String secondsStr = (seconds % 60).toString().padLeft(2, '0');
+
+    return "$hoursStr:$minutesStr:$secondsStr";
   }
 }
