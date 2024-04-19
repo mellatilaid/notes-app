5278a1ea92c557fe09cf5c0687e37ecde64c8307
mellatilaid <laidmellati@gmail.com>
2024-04-19 23:20:16 +0200

    sperated voice player view body in a seperate widget

--- lib/views/voice_player_view.dart
+++ lib/views/voice_player_view.dart
@@ -1,19 +1,23 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/cubits/voice_notes_cubits_folder/voice_notes_cubit/voice_notes_cubit.dart';
-import 'package:note_app/helper/const.dart';
 import 'package:note_app/models/voice_note_model.dart';
-import 'package:note_app/widgets/custom_rounded_icon.dart';
-import 'package:note_app/widgets/custom_text_field.dart';
 
-class VoicePlayerView extends StatelessWidget {
+import '../widgets/voice_player_view_body.dart';
+
+class VoicePlayerView extends StatefulWidget {
   final VoiceNoteModel voiceNote;
   const VoicePlayerView({
     super.key,
     required this.voiceNote,
   });
 
+  @override
+  State<VoicePlayerView> createState() => _VoicePlayerViewState();
+}
+
+class _VoicePlayerViewState extends State<VoicePlayerView> {
+  bool isReadOnly = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +27,9 @@ class VoicePlayerView extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {
-              _deleteNote(context: context);
+              setState(() {
+                isReadOnly = !isReadOnly;
+              });
             },
             icon: const Icon(
               Icons.edit,
@@ -37,204 +43,26 @@ class VoicePlayerView extends StatelessWidget {
               Icons.delete_forever,
             ),
           ),
+          !isReadOnly
+              ? IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.done,
+                  ),
+                )
+              : const SizedBox(),
         ],
       ),
-      body: VoicePlayerViewBody(voiceNote: voiceNote),
+      body: VoicePlayerViewBody(
+        isReadOnly: isReadOnly,
+        voiceNote: widget.voiceNote,
+      ),
     );
-  }
-
-  String formatDuration(int milliseconds) {
-    int seconds = (milliseconds / 1000).floor();
-    int minutes = (seconds / 60).floor();
-    int hours = (minutes / 60).floor();
-
-    String hoursStr = (hours % 24).toString().padLeft(2, '0');
-    String minutesStr = (minutes % 60).toString().padLeft(2, '0');
-    String secondsStr = (seconds % 60).toString().padLeft(2, '0');
-
-    return "$hoursStr:$minutesStr:$secondsStr";
   }
 
   _deleteNote({required BuildContext context}) {
     Navigator.pop(context);
-    voiceNote.delete();
+    widget.voiceNote.delete();
     BlocProvider.of<VoiceNotesCubit>(context).fetchVoiceNotes();
-  }
-}
-
-class VoicePlayerViewBody extends StatefulWidget {
-  final VoiceNoteModel voiceNote;
-  const VoicePlayerViewBody({super.key, required this.voiceNote});
-
-  @override
-  State<VoicePlayerViewBody> createState() => _VoicePlayerViewBodyState();
-}
-
-class _VoicePlayerViewBodyState extends State<VoicePlayerViewBody> {
-  late TextEditingController _titleController;
-  final audioPlayer = AudioPlayer();
-  bool isPlaying = false;
-  var duration = Duration.zero;
-  var position = Duration.zero;
-  bool _isCompleted = false;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _titleController = TextEditingController(text: widget.voiceNote.title);
-    setAudio();
-    audioPlayer.onPlayerStateChanged.listen((state) {
-      if (mounted) {
-        setState(() {
-          isPlaying = state == PlayerState.playing;
-        });
-      }
-    });
-
-    audioPlayer.onPlayerComplete.listen((event) {
-      if (mounted) {
-        isPlaying = false;
-        position = Duration.zero;
-        _isCompleted = true;
-      }
-    });
-
-    audioPlayer.onDurationChanged.listen((newDuration) {
-      if (mounted) {
-        setState(() {
-          duration = newDuration;
-        });
-      }
-    });
-
-    audioPlayer.onPositionChanged.listen((newPostion) {
-      if (mounted) {
-        setState(() {
-          position = newPostion;
-        });
-      }
-    });
-  }
-
-  setAudio() async {
-    try {
-      audioPlayer.setReleaseMode(ReleaseMode.loop);
-      audioPlayer.setSourceUrl(widget.voiceNote.voicePath);
-    } catch (e) {
-      throw Exception(e.toString());
-    }
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    _titleController.dispose();
-    audioPlayer.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          CustomTextField(
-            readOnly: true,
-            textStyle: const TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-            controller: _titleController,
-            hintText: 'Title',
-          ),
-          const Spacer(
-            flex: 1,
-          ),
-          Container(
-            margin: const EdgeInsets.all(8),
-            width: double.infinity,
-            height: 200,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              image: const DecorationImage(
-                image: AssetImage(
-                  'assets/audio.png',
-                ),
-                fit: BoxFit.fill,
-                filterQuality: FilterQuality.high,
-              ),
-            ),
-          ),
-          Card(
-            elevation: 5,
-            child: Slider(
-              activeColor: kPrimaryColor,
-              min: 0,
-              max: duration.inMicroseconds.toDouble(),
-              value: position.inMicroseconds.toDouble(),
-              onChanged: (value) async {
-                final postion = Duration(microseconds: value.toInt());
-                await audioPlayer.seek(postion);
-
-                await audioPlayer.resume();
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(formatDuration(position.inMilliseconds)),
-                Text(formatDuration(duration.inMilliseconds)),
-              ],
-            ),
-          ),
-          const Spacer(
-            flex: 2,
-          ),
-          CustomRoundedIcon(
-            onTap: () async {
-              if (isPlaying) {
-                await audioPlayer.pause();
-                setState(() {
-                  isPlaying = false;
-                });
-              } else {
-                if (_isCompleted) {
-                  await audioPlayer.seek(
-                      Duration.zero); // Reset the audio position to the start
-                  _isCompleted = false; // Reset the completion flag
-                }
-                await audioPlayer.resume();
-                setState(
-                  () {
-                    isPlaying = true;
-                  },
-                );
-              }
-            },
-            size: 60,
-            icon: isPlaying ? Icons.stop : Icons.play_arrow,
-          ),
-          const Spacer(
-            flex: 1,
-          ),
-        ],
-      ),
-    );
-  }
-
-  String formatDuration(int milliseconds) {
-    int seconds = (milliseconds / 1000).floor();
-    int minutes = (seconds / 60).floor();
-    int hours = (minutes / 60).floor();
-
-    String hoursStr = (hours % 24).toString().padLeft(2, '0');
-    String minutesStr = (minutes % 60).toString().padLeft(2, '0');
-    String secondsStr = (seconds % 60).toString().padLeft(2, '0');
-
-    return "$hoursStr:$minutesStr:$secondsStr";
   }
 }
---
+++ lib/widgets/voice_player_view_body.dart
@@ -0,0 +1,186 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+import '../helper/const.dart';
+import '../models/voice_note_model.dart';
+import 'custom_rounded_icon.dart';
+import 'custom_text_field.dart';
+
+class VoicePlayerViewBody extends StatefulWidget {
+  final VoiceNoteModel voiceNote;
+  final bool isReadOnly;
+  const VoicePlayerViewBody(
+      {super.key, required this.voiceNote, this.isReadOnly = true});
+
+  @override
+  State<VoicePlayerViewBody> createState() => _VoicePlayerViewBodyState();
+}
+
+class _VoicePlayerViewBodyState extends State<VoicePlayerViewBody> {
+  late TextEditingController _titleController;
+  final audioPlayer = AudioPlayer();
+  bool isPlaying = false;
+  var duration = Duration.zero;
+  var position = Duration.zero;
+  bool _isCompleted = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _titleController = TextEditingController(text: widget.voiceNote.title);
+    setAudio();
+    audioPlayer.onPlayerStateChanged.listen((state) {
+      if (mounted) {
+        setState(() {
+          isPlaying = state == PlayerState.playing;
+        });
+      }
+    });
+
+    audioPlayer.onPlayerComplete.listen((event) {
+      if (mounted) {
+        isPlaying = false;
+        position = Duration.zero;
+        _isCompleted = true;
+      }
+    });
+
+    audioPlayer.onDurationChanged.listen((newDuration) {
+      if (mounted) {
+        setState(() {
+          duration = newDuration;
+        });
+      }
+    });
+
+    audioPlayer.onPositionChanged.listen((newPostion) {
+      if (mounted) {
+        setState(() {
+          position = newPostion;
+        });
+      }
+    });
+  }
+
+  setAudio() async {
+    try {
+      audioPlayer.setReleaseMode(ReleaseMode.loop);
+      audioPlayer.setSourceUrl(widget.voiceNote.voicePath);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _titleController.dispose();
+    audioPlayer.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          CustomTextField(
+            readOnly: widget.isReadOnly,
+            textStyle: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            controller: _titleController,
+            hintText: 'Title',
+          ),
+          const Spacer(
+            flex: 1,
+          ),
+          Container(
+            margin: const EdgeInsets.all(8),
+            width: double.infinity,
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              image: const DecorationImage(
+                image: AssetImage(
+                  'assets/audio.png',
+                ),
+                fit: BoxFit.fill,
+                filterQuality: FilterQuality.high,
+              ),
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Slider(
+              activeColor: kPrimaryColor,
+              min: 0,
+              max: duration.inMicroseconds.toDouble(),
+              value: position.inMicroseconds.toDouble(),
+              onChanged: (value) async {
+                final postion = Duration(microseconds: value.toInt());
+                await audioPlayer.seek(postion);
+
+                await audioPlayer.resume();
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(formatDuration(position.inMilliseconds)),
+                Text(formatDuration(duration.inMilliseconds)),
+              ],
+            ),
+          ),
+          const Spacer(
+            flex: 2,
+          ),
+          CustomRoundedIcon(
+            onTap: () async {
+              if (isPlaying) {
+                await audioPlayer.pause();
+                setState(() {
+                  isPlaying = false;
+                });
+              } else {
+                if (_isCompleted) {
+                  await audioPlayer.seek(
+                      Duration.zero); // Reset the audio position to the start
+                  _isCompleted = false; // Reset the completion flag
+                }
+                await audioPlayer.resume();
+                setState(
+                  () {
+                    isPlaying = true;
+                  },
+                );
+              }
+            },
+            size: 60,
+            icon: isPlaying ? Icons.stop : Icons.play_arrow,
+          ),
+          const Spacer(
+            flex: 1,
+          ),
+        ],
+      ),
+    );
+  }
+
+  String formatDuration(int milliseconds) {
+    int seconds = (milliseconds / 1000).floor();
+    int minutes = (seconds / 60).floor();
+    int hours = (minutes / 60).floor();
+
+    String hoursStr = (hours % 24).toString().padLeft(2, '0');
+    String minutesStr = (minutes % 60).toString().padLeft(2, '0');
+    String secondsStr = (seconds % 60).toString().padLeft(2, '0');
+
+    return "$hoursStr:$minutesStr:$secondsStr";
+  }
+}
