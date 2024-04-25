9a77be9c82b79834294ece4d89f467aabc3c4ad8
mellatilaid <laidmellati@gmail.com>
2024-04-25 15:09:51 +0200

    generated image note model adaptor

--- lib/models/image_note_model.dart
+++ lib/models/image_note_model.dart
@@ -1,5 +1,12 @@
-class ImageNoteModel {
+import 'package:hive_flutter/adapters.dart';
+
+part 'image_note_model.g.dart';
+
+@HiveType(typeId: 3)
+class ImageNoteModel extends HiveObject {
+  @HiveField(0)
   final String? imageTitle;
+  @HiveField(1)
   final String imagePath;
 
   ImageNoteModel({
