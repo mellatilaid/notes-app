ef25456f58a7fd52a2539737923d10234d09eb4d
mellatilaid <laidmellati@gmail.com>
2024-04-26 15:52:48 +0200

    creaded pick image method in add image note bottom sheet body

--- lib/widgets/add_image_note_bottom_sheet_body.dart
+++ lib/widgets/add_image_note_bottom_sheet_body.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/section_divider_with_title.dart';
 
 import '../helper/const.dart';
+import '../helper/image_helper.dart';
 import 'custom_action_button.dart';
 import 'custom_text_button.dart';
 import 'dual_action_text_field.dart';
@@ -18,43 +21,73 @@ class _AddImageNoteBottomSheetBodyState
     extends State<AddImageNoteBottomSheetBody> {
   final TextEditingController imageNoteTitleController =
       TextEditingController();
-
+  String? imageNotePath;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          SizedBox(
-            height: 350,
-            child: Card(
-              elevation: 10,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.asset('assets/text.png'),
+      padding: EdgeInsets.only(
+        top: 16,
+        left: 16,
+        right: 16,
+        bottom: MediaQuery.of(context).viewInsets.bottom + 16,
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            SizedBox(
+              height: 350,
+              child: Card(
+                elevation: 10,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: (imageNotePath != null)
+                      ? Image.file(
+                          File(imageNotePath!),
+                          fit: BoxFit.contain,
+                        )
+                      : Image.asset('assets/text.png'),
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 16),
-          DualActionTextField(
-            controller: imageNoteTitleController,
-            hintText: 'Enter image title',
-          ),
-          const SizedBox(height: 10),
-          const SectionDividerWithTitle(title: 'Upload image'),
-          const SizedBox(height: 8),
-          CustomTextButton(title: 'Upload Image', onPressed: () {}),
-          const SizedBox(
-            height: 16,
-          ),
-          CustomActionButton(
-            title: 'Add Image',
-            onPressed: () {},
-            backGroundColor: kPrimaryColor,
-          ),
-        ],
+            const SizedBox(height: 16),
+            DualActionTextField(
+              controller: imageNoteTitleController,
+              hintText: 'Enter image title',
+            ),
+            const SizedBox(height: 10),
+            const SectionDividerWithTitle(title: 'Upload image'),
+            const SizedBox(height: 8),
+            CustomTextButton(
+                title: 'Upload Image',
+                onPressed: () async {
+                  final imagePath = await _imagePicker(context: context);
+                  if (imagePath != null) {
+                    setState(() {
+                      imageNotePath = imagePath;
+                    });
+                  }
+                }),
+            const SizedBox(
+              height: 16,
+            ),
+            CustomActionButton(
+              title: 'Add Image',
+              onPressed: () {},
+              backGroundColor: kPrimaryColor,
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  _imagePicker({required BuildContext context}) async {
+    final pickedImage = await ImageHelper().pickImage();
+
+    if (pickedImage != null) {
+      return pickedImage.path;
+    }
+    return null;
   }
 }
