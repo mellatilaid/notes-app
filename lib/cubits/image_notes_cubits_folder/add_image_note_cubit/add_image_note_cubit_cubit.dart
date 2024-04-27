7e2d19bdfb0047f7513ebb5a60e800131e151340
mellatilaid <laidmellati@gmail.com>
2024-04-27 11:16:34 +0200

    created add image note cubit states

--- lib/cubits/image_notes_cubits_folder/add_image_note_cubit/add_image_note_cubit_cubit.dart
+++ lib/cubits/image_notes_cubits_folder/add_image_note_cubit/add_image_note_cubit_cubit.dart
@@ -1,8 +1,8 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'add_image_note_cubit_state.dart';
 
 class AddImageNoteCubitCubit extends Cubit<AddImageNoteCubitState> {
-  AddImageNoteCubitCubit() : super(AddImageNoteCubitInitial());
+  AddImageNoteCubitCubit() : super(AddImageNoteInitialState());
 }
--- lib/cubits/image_notes_cubits_folder/add_image_note_cubit/add_image_note_cubit_state.dart
+++ lib/cubits/image_notes_cubits_folder/add_image_note_cubit/add_image_note_cubit_state.dart
@@ -3,4 +3,13 @@ part of 'add_image_note_cubit_cubit.dart';
 @immutable
 sealed class AddImageNoteCubitState {}
 
-final class AddImageNoteCubitInitial extends AddImageNoteCubitState {}
+final class AddImageNoteInitialState extends AddImageNoteCubitState {}
+
+final class AddImageNoteLoadingState extends AddImageNoteCubitState {}
+
+final class AddImgeNoteSuccusState extends AddImageNoteCubitState {}
+
+final class AddImageNoteFailedState extends AddImageNoteCubitState {
+  final String errMessage;
+  AddImageNoteFailedState({required this.errMessage});
+}
