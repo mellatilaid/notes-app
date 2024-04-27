cc66c47bda48035ef9d1fe6a206dfb23c2c0f0c3
mellatilaid <laidmellati@gmail.com>
2024-04-27 12:34:43 +0200

    created fetch image notes states

--- lib/cubits/image_notes_cubits_folder/image_notes_cubit/image_notes_cubit_cubit.dart
+++ lib/cubits/image_notes_cubits_folder/image_notes_cubit/image_notes_cubit_cubit.dart
@@ -1,8 +1,9 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/models/image_note_model.dart';
 
 part 'image_notes_cubit_state.dart';
 
 class ImageNotesCubitCubit extends Cubit<ImageNotesCubitState> {
-  ImageNotesCubitCubit() : super(ImageNotesCubitInitial());
+  ImageNotesCubitCubit() : super((ImageNotesInitialState()));
 }
--- lib/cubits/image_notes_cubits_folder/image_notes_cubit/image_notes_cubit_state.dart
+++ lib/cubits/image_notes_cubits_folder/image_notes_cubit/image_notes_cubit_state.dart
@@ -3,4 +3,9 @@ part of 'image_notes_cubit_cubit.dart';
 @immutable
 sealed class ImageNotesCubitState {}
 
-final class ImageNotesCubitInitial extends ImageNotesCubitState {}
+final class ImageNotesInitialState extends ImageNotesCubitState {}
+
+final class ImageNotesSucusState extends ImageNotesCubitState {
+  final List<ImageNoteModel> imageNotes;
+  ImageNotesSucusState({required this.imageNotes});
+}
