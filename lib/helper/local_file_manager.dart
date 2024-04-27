6bd2f35a69887866050912524665ceda3381bbb8
mellatilaid <laidmellati@gmail.com>
2024-04-27 11:06:46 +0200

    added savefiletolocal method logic

--- lib/helper/local_file_manager.dart
+++ lib/helper/local_file_manager.dart
@@ -1,5 +1,35 @@
-class LocalFileManager {
-  saveFileToLocal() {}
+import 'dart:io';
 
-  removeFileFromLocal() {}
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
+
+class LocalFileManager {
+  final String filePath;
+  const LocalFileManager({required this.filePath});
+
+  saveFileToLocal() async {
+    final File file = File(filePath);
+    //get the application document directory
+    final directory = await getApplicationDocumentsDirectory();
+    //create a unique name for the file to be saved
+    final fileName = path.basename(filePath);
+    //copy the file to app's local directory
+    final localFilePath = path.join(directory.path, fileName);
+    final localFile = await file.copy(localFilePath);
+    //return the local saved file path
+    return localFile.path;
+  }
+
+  removeFileFromLocal() async {
+    final File file = File(filePath);
+    try {
+      if (await file.exists()) {
+        await file.delete();
+      } else {
+        print('file does not exist');
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
 }
