ee24461a452f60e5cde8965a8063cd4b55bd3625
mellatilaid <laidmellati@gmail.com>
2024-04-27 12:56:24 +0200

    added bloc builder in image notes view body

--- lib/widgets/image_notes_view_body.dart
+++ lib/widgets/image_notes_view_body.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/image_notes_cubits_folder/image_notes_cubit/image_notes_cubit_cubit.dart';
+import 'package:note_app/widgets/empty_widget.dart';
 
 import 'image_notes_list_view.dart';
 
-class ImageNotesViewBody extends StatelessWidget {
+class ImageNotesViewBody extends StatefulWidget {
   const ImageNotesViewBody({super.key});
+
+  @override
+  State<ImageNotesViewBody> createState() => _ImageNotesViewBodyState();
+}
+
+class _ImageNotesViewBodyState extends State<ImageNotesViewBody> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    BlocProvider.of<ImageNotesCubit>(context).fetchAllImageNotes();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +31,24 @@ class ImageNotesViewBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ImageNotesListView();
+    return BlocBuilder<ImageNotesCubit, ImageNotesCubitState>(
+      builder: (context, state) {
+        if (state is ImageNotesSucusState) {
+          if (state.imageNotes.isEmpty) {
+            return const EmptyWidget(
+              title: 'Image notes is empty',
+              message: 'Add your first image note',
+              imagePath: '',
+            );
+          } else {
+            return const ImageNotesListView();
+          }
+        } else {
+          return const Center(
+            child: Text('oops there was a problem'),
+          );
+        }
+      },
+    );
   }
 }
