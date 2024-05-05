397a8e825f75e53d137120031fa1032a3406d4d3
mellatilaid <laidmellati@gmail.com>
2024-05-05 11:53:10 +0200

    generate to do item model type adaptor

--- lib/models/to_do_item_model.dart
+++ lib/models/to_do_item_model.dart
@@ -1,5 +1,12 @@
-class ToDoItemModel {
+import 'package:hive/hive.dart';
+
+part 'to_do_item_model.g.dart';
+
+@HiveType(typeId: 4)
+class ToDoItemModel extends HiveObject {
+  @HiveField(0)
   String? title;
+  @HiveField(1)
   bool isChecked = false;
 
   ToDoItemModel({this.title, required this.isChecked});
