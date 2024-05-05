455c4622091a1d304d380c4c72854985e35f98c0
mellatilaid <laidmellati@gmail.com>
2024-05-05 10:43:29 +0200

    added to to do alert dialog body to do list checklist

--- lib/widgets/to_do_alert_dialog_body.dart
+++ lib/widgets/to_do_alert_dialog_body.dart
@@ -9,7 +9,19 @@ class ToDoAlertDialogBody extends StatefulWidget {
 }
 
 class _ToDoAlertDialogBodyState extends State<ToDoAlertDialogBody> {
+  List<String> toDoItems = [];
   final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _toDoItemController = TextEditingController();
+  final FocusNode foucusNode = FocusNode();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _titleController.dispose();
+    _toDoItemController.dispose();
+    foucusNode.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -24,24 +36,36 @@ class _ToDoAlertDialogBodyState extends State<ToDoAlertDialogBody> {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                for (int i = 0; i < 2; i++)
+                for (int i = 0; i < toDoItems.length; i++)
                   CheckboxListTile(
                     value: false,
                     onChanged: (bool? value) {
                       if (value!) {
                         setState(
-                          () {},
+                          () {
+                            toDoItems.removeAt(i);
+                          },
                         );
                       }
                     },
-                    title: const Text('laid'),
+                    title: Text(toDoItems[i]),
                   ),
                 TextField(
+                  focusNode: foucusNode,
+                  controller: _toDoItemController,
                   decoration: const InputDecoration(
                     hintText: 'Enter to-do item',
                   ),
                   textInputAction: TextInputAction.next,
-                  onSubmitted: (String value) {},
+                  onSubmitted: (String value) {
+                    setState(
+                      () {
+                        toDoItems.add(value);
+                      },
+                    );
+                    _toDoItemController.clear();
+                    FocusScope.of(context).requestFocus(foucusNode);
+                  },
                 )
               ],
             ),
@@ -52,7 +76,14 @@ class _ToDoAlertDialogBodyState extends State<ToDoAlertDialogBody> {
         ElevatedButton(
           child: const Text('Close'),
           onPressed: () {
+            if (foucusNode.hasFocus) {
+              FocusScope.of(context).unfocus();
+            }
+
             Navigator.pop(context);
+
+            _toDoItemController.clear();
+            toDoItems.clear();
           },
         ),
       ],
