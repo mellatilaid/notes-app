a732797a5f4800eb78144bc51a2d022058e31bac
mellatilaid <laidmellati@gmail.com>
2024-05-05 12:07:34 +0200

    regestred tasks list type adaptor and open its box

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:note_app/cubits/voice_notes_cubits_folder/voice_notes_cubit/voic
 import 'package:note_app/helper/const.dart';
 import 'package:note_app/models/folder_model.dart';
 import 'package:note_app/models/image_note_model.dart';
+import 'package:note_app/models/tasks_list_model.dart';
 import 'package:note_app/models/to_do_item_model.dart';
 import 'package:note_app/models/voice_note_model.dart';
 
@@ -30,7 +31,8 @@ void main() async {
   Hive.registerAdapter(ImageNoteModelAdapter());
   await Hive.openBox<ImageNoteModel>(kImageNoteBox);
   Hive.registerAdapter(ToDoItemModelAdapter());
-
+  Hive.registerAdapter(TasksListModelAdapter());
+  Hive.openBox<TasksListModel>(kTasksListBox);
   runApp(const NotesApp());
 }
 
