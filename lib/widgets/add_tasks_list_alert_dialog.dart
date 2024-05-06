836138c6a79350299f69e34d033e9e502e3e6817
mellatilaid <laidmellati@gmail.com>
2024-05-06 10:36:52 +0200

    provided add tasks list cubit to add tasks list alert dialog

--- lib/widgets/add_tasks_list_alert_dialog.dart
+++ lib/widgets/add_tasks_list_alert_dialog.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/tasks_lists_cubits/cubit/add_tasks_list_cubit.dart';
 import 'package:note_app/widgets/add_tasks_list_alert_dialog_body.dart';
 
 class AddTasksListAlertDialog extends StatelessWidget {
@@ -6,6 +8,9 @@ class AddTasksListAlertDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const AddTasksListAlertDialogBody();
+    return BlocProvider(
+      create: (context) => AddTasksListCubit(),
+      child: const AddTasksListAlertDialogBody(),
+    );
   }
 }
