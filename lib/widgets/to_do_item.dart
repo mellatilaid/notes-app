7fef3fa5cbd5df33072cef055882dbd89ac7e12e
mellatilaid <laidmellati@gmail.com>
2024-05-11 09:55:36 +0200

    created a fucntion in to do item to decide whether to show dots under tasks or not based on tasks list length

--- lib/widgets/to_do_item.dart
+++ lib/widgets/to_do_item.dart
@@ -15,13 +15,15 @@ class ToDoItem extends StatefulWidget {
 
 class _ToDoItemState extends State<ToDoItem> {
   final TextEditingController _title = TextEditingController();
-
+  int loopLength = 0;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     log(widget.tasksList.title ?? '');
     _title.text = widget.tasksList.title ?? '';
+    loopLength =
+        _setLoopLength(taskListLength: widget.tasksList.tasksList.length);
   }
 
   @override
@@ -32,28 +34,57 @@ class _ToDoItemState extends State<ToDoItem> {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          InvisibleTextField(controller: _title),
-          for (int i = 0; i < 2; i++)
+          _title.text.isNotEmpty
+              ? InvisibleTextField(
+                  controller: _title,
+                  textStyle: Theme.of(context).textTheme.headlineMedium,
+                )
+              : const SizedBox(),
+          for (int i = 0; i < loopLength; i++)
             CheckboxListTile(
               controlAffinity: ListTileControlAffinity.leading,
-              value: false,
+              value: widget.tasksList.tasksList[i].isChecked,
               onChanged: (bool? value) {
                 if (value!) {}
               },
-              title: const Text('laid'),
+              title: Text(widget.tasksList.tasksList[i].title),
             ),
-          const Padding(
-            padding: EdgeInsets.only(left: 24, bottom: 8),
-            child: Text(
-              '...',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-          ),
+          _isShowDots(tasksListLength: widget.tasksList.tasksList.length)
+              ? const Padding(
+                  padding: EdgeInsets.only(left: 24, bottom: 8),
+                  child: Text(
+                    '...',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                )
+              : const SizedBox(),
         ],
       ),
     );
+  }
+
+  //sets the length of the loop
+  //so if the taskslist greate length greater than 2 set length to 2
+  //otherways let it as it is
+  _setLoopLength({required int taskListLength}) {
+    if (taskListLength > 2) {
+      return 2;
+    } else {
+      return taskListLength;
+    }
+  }
+
+//check tasks list length if greater than 2 returns true to show dots
+//to tell the user there is more tasks
+//otherways return false
+  bool _isShowDots({required int tasksListLength}) {
+    if (tasksListLength > 2) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
