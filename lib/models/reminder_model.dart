fe13160ab88e906aeef16a0ede4391478b6b4c81
mellatilaid <laidmellati@gmail.com>
2024-05-27 17:43:49 +0200

    generated reminder model type adaptor

--- lib/models/reminder_model.dart
+++ lib/models/reminder_model.dart
@@ -1,6 +1,14 @@
-class ReminderModel {
+import 'package:hive/hive.dart';
+
+part 'reminder_model.g.dart';
+
+@HiveType(typeId: 6)
+class ReminderModel extends HiveObject {
+  @HiveField(0)
   int id;
+  @HiveField(1)
   String title;
+  @HiveField(2)
   String date;
 
   ReminderModel({required this.id, required this.title, required this.date});
