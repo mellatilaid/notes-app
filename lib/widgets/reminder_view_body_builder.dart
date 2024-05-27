f5d68bb195d6e95c09e6902ee2b4d8e995f0a1eb
mellatilaid <laidmellati@gmail.com>
2024-05-27 19:21:24 +0200

    created reminders list view

--- lib/widgets/reminder_view_body_builder.dart
+++ lib/widgets/reminder_view_body_builder.dart
@@ -8,20 +8,3 @@ class RemindersViewBodyBuilder extends StatelessWidget {
     return const Placeholder();
   }
 }
-
-class RemindersListView extends StatelessWidget {
-  const RemindersListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: const BouncingScrollPhysics(),
-      key: UniqueKey(),
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return const Placeholder();
-      },
-    );
-  }
-}
---
+++ lib/widgets/reminders_list_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RemindersListView extends StatelessWidget {
+  const RemindersListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      key: UniqueKey(),
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Placeholder();
+      },
+    );
+  }
+}
