15accc5d978bb1b1398db83d62c894a4da449985
mellatilaid <laidmellati@gmail.com>
2024-05-27 18:42:41 +0200

    created a function that generates unique id for the reminder id

--- lib/helper/generate_unique_id.dart
+++ lib/helper/generate_unique_id.dart
@@ -1,1 +1,12 @@
-generateUniqueId() {}
+import 'package:uuid/uuid.dart';
+
+int generateUniqueId() {
+  // Create a UUID object
+  var uuid = const Uuid();
+
+// Generate a v4 (random) UUID
+  String uniqueId = uuid.v4();
+
+// Use the hash of the UUID as the unique integer ID for the notification
+  return uniqueId.hashCode;
+}
