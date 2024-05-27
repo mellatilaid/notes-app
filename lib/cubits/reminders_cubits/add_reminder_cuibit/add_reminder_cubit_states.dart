ee4b656b28623bf566282a9c62a68c898577fd11
mellatilaid <laidmellati@gmail.com>
2024-05-27 17:49:27 +0200

    created add reminder cubit states

---
+++ lib/cubits/reminders_cubits/add_reminder_cuibit/add_reminder_cubit_states.dart
@@ -0,0 +1,9 @@
+class AddReminderState {}
+
+final class AddReminderInitial extends AddReminderState {}
+
+final class AddReminderLoading extends AddReminderState {}
+
+final class AddReminderSuccuss extends AddReminderState {}
+
+final class AddReminderFailure extends AddReminderState {}
