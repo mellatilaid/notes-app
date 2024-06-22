c5f400f9ac69f968cdaa70e9e9a18a4e019b57ef
mellatilaid <laidmellati@gmail.com>
2024-06-22 10:50:01 +0200

    removed feedback_gitlab package

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ import 'package:note_app/models/tasks_list_model.dart';
 import 'package:note_app/models/to_do_item_model.dart';
 import 'package:note_app/models/voice_note_model.dart';
 import 'package:note_app/services/local_notifications_service.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'models/note_model.dart';
 
@@ -41,7 +42,20 @@ void main() async {
   Hive.registerAdapter(ReminderModelAdapter());
   await Hive.openBox<ReminderModel>(kRemindersBox);
   await LocalNotifications.init();
-  runApp(const NotesApp());
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://068124ccff399313e15a047502cddfa8@o4506723427352576.ingest.us.sentry.io/4507463934148608';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(const NotesApp()),
+  );
+  //runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
